e5a0b5a6fa00603cc509143495b07f695ce80bde
Rajarajacholan11 <149559911+Rajarajacholan11@users.noreply.github.com>
2024-08-12 12:48:33 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,50 @@
+
+const  i1=document.getElementById("i1");
+const  r1=document.getElementById("r1");
+i1.addEventListener("click",()=>
+{
+r1.classList.toggle('hide');
+console.log("hide");
+
+})
+
+
+const  i2=document.getElementById("i2");
+const  r2=document.getElementById("r2");
+i2.addEventListener("click",()=>
+{
+r2.classList.toggle('hide');
+console.log("hide");
+
+})
+
+
+const  i3=document.getElementById("i3");
+const  r3=document.getElementById("r3");
+i3.addEventListener("click",()=>
+{
+r3.classList.toggle('hide');
+console.log("hide");
+
+})
+
+
+
+const  i4=document.getElementById("i4");
+const  r4=document.getElementById("r4");
+i4.addEventListener("click",()=>
+{
+r4.classList.toggle('hide');
+console.log("hide");
+
+})
+
+
+const  i5=document.getElementById("i5");
+const  r5=document.getElementById("r5");
+i5.addEventListener("click",()=>
+{
+r5.classList.toggle('hide');
+console.log("hide");
+
+})
